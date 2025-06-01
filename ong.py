33a101133c6af65be21f2ca558456406a16fb155
Jorge201105 <jimunozacuna@gmail.com>
2025-06-01 06:09:34 +0000

    terminado

--- ong.py
+++ ong.py
@@ -18,15 +18,18 @@ def productoria(lista):
 
 def calcular(**kwargs):
     
-   
+   # se utiliza  el echo que el diccionario reconoce cuando entregamos palabras y numeros y 
+   # los asocia automaticamente a las variables clave-valor que son representadas como k,v
+   # 
     for k,v in kwargs.items():
         if "fact" in k:
             print (f"El factorial de {v} es {factorial(v)} ")
             
         else: 
             print(f"La productoria de {v} es {productoria(v)} ")       
-   
-    
+   # no coloque return porque no supe como colocarlo para que me lo mostrara como me lo pedían , sin embargo, solo con print me daba la posibilidad
+   #  de dejarlo como me lo pedian.
+   # no puse alternativas de si se escribia mal la informacion, debido a que la informacion ya esta escrita. 
     
 
 calcular(fact_1=5, prod_1=[3,6,4,2,8], fact_2=6)            
